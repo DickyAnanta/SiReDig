3f95e3b82e37b4e5eb95cda882480d1ca7fc6439
V4NT4 <ivanta788@gmail.com>
2023-02-19 21:15:09 +0700

    edit navbar dan tambah login_view

---
+++ app/Views/Login/login_view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="/assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/assets/adminlte/css/adminlte.min.css">
+  <!-- css saya -->
+  <link rel="stylesheet" href="/css/login.css">
+</head>
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="card-body login-card-body">
+        <a href=""><i class="fas fa-arrow-left"></i></a>
+        <form action="index3.html" method="post">
+          <img src="/assets/img/loogo.png" alt="">
+          <div class="form-group">
+            <p for="exampleInputEmail1">Email address</p>
+            <input type="email" class="" placeholder="masukkan username" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="form-group">
+            <p for="exampleInputPassword1">Password</p>
+            <input type="password" class="" placeholder="masukkan password" id="exampleInputPassword1">
+          </div>
+          <div class="row">
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn">Masuk</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+      </div>
+      <!-- /.login-card-body -->
+    </div>
+  </div>
+  <!-- /.login-box -->
+
+  <!-- jQuery -->
+  <script src="/assets/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="/assets/adminlte/js/adminlte.min.js"></script>
+</body>
+
+</html>
