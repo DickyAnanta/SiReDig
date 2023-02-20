c22c739423dda0f33fa661e5f26087573cb56e37
Dicky Ananta <dickycah.krasak@gmail.com>
2023-02-20 21:20:03 +0700

    login

---
+++ app/Controllers/Login.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\userModel;
+
+class Login extends BaseController
+{
+  public function index()
+  {
+    return view('login.php');
+  }
+
+  public function Login()
+  {
+    $username = $this->request->getVar('username');
+    dd($username);
+    $password = $this->request->getVar('password');
+
+    $user = new userModel();
+    $data = $user->where('username', $username)->first();
+    dd($data);
+    if ($data) {
+      $session = session();
+      if (password_verify($password, $data['password'])) {
+        //create session
+        $login = [
+          'islogin' => true,
+          'username' => $data['username'],
+          'name' => $data['name']
+        ];
+        $session->set($login);
+        return redirect()->to('/home');
+      } else {
+        $session->setFlashdata('msg', 'Email/Password invalid');
+        return redirect()->to('/login');
+      }
+    }
+  }
+
+  public function logout()
+  {
+    $session = session();
+    $session->destroy();
+    return redirect()->to('/login');
+  }
+}
---
+++ app/Models/loginModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class loginModel extends Model
+{
+  protected $tabellogin = 'user';
+  protected $primary_column = 'username';
+}
